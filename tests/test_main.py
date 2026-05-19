ef341cc45bad3cbda897b67069f861acf337e1be
Murtaza Hiraj <murtazahiraj@berkeley.edu>
2026-05-19 17:13:50 +0500

    added smoke test to main.py for new method

--- tests/test_main.py
+++ tests/test_main.py
@@ -26,3 +26,13 @@ class TestMain:
             text=True
         )
         assert result.returncode == 1
+
+    def test_cli_range_basic(self):
+        result = subprocess.run(
+            ["python", "main.py", "most_active_cookie_for_range", "tests/log_file_fixtures/simple_case.csv",
+             "--start_date", "2018-12-08", "--end_date", "2018-12-09"],
+            capture_output=True,
+            text=True
+        )
+        assert result.returncode == 0
+        assert "AtY0laUfhglK31C7" in result.stdout
